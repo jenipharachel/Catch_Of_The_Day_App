ce1b6e477dcfad6cf29808bb716e7cfdb4c75f03
jenipha <jenipharachel@gmail.com>
2019-08-30 21:55:07 +0530

    updated storename with helper function

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,5 +1,9 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React from "react"; /*Default import*/
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from "react-router-dom"; /*named import*/
 import StorePicker from "./StorePicker";
 import App from "./App";
 import NotFound from "./NotFound";
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { getFunName } from "../helpers";
+import { get } from "https";
 
 class StorePicker extends React.Component {
   render() {
     return (
       <form className="store-selector">
         <h2>Enter a Store name</h2>
-        <input type="text" placeholder="Store Name" required />
+        <input
+          type="text"
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+          required
+        />
         <button type="submit">Visit Store</button>
       </form>
     );
