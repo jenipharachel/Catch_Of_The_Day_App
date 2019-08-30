bc46680a89be121e285b9e6ec984f2720fd13b16
jenipha <jenipharachel@gmail.com>
2019-08-30 21:34:57 +0530

    updated routing

---
+++ src/components/NotFound.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const NotFound = () => <h2>PAGE NOT FOUND</h2>;
+
+export default NotFound;
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,18 +1,17 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import StorePicker from "./StorePicker";
+import App from "./App";
+import NotFound from "./NotFound";
 
-class Router extends React.Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route exactpath="/" />
-          <Route path="/store/`${storeName}`" />
-          <Route />
-        </Switch>
-      </BrowserRouter>
-    );
-  }
-}
+const Router = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={StorePicker} />
+      <Route path="/store/:storeId" component={App} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default Router;
